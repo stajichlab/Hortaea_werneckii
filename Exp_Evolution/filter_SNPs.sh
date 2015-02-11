105b8a32c5d6876d562b98a65ac3626f1f205680
Jason Stajich <jason@bioperl.org>
2015-02-10 19:40:48 -0800

    filter snps

---
+++ Exp_Evolution/filter_SNPs.sh
@@ -0,0 +1,11 @@
+
+module load GATK
+module load java
+
+GENOME=/shared/stajichlab/projects/Hortaea_werneckii/assemblies/Hw2/Hw2.fasta
+java -Xmx2g -jar GenomeAnalysisTK.jar \
+   -R $GENOME 
+   -T SelectVariants \
+   --variant ALL.gatk.snp.hardfilter.vcf \
+   -o \LL.gatk.snp.selected.vcf 
+   --excludeFiltered
