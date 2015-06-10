4050e06182ba19904c91df1e6d29faa2c3840295
Jason Stajich <jason@bioperl.org>
2015-06-09 19:44:43 -0700

    Hortaea RNAseq analysis for multiplexed merged
    Former-commit-id: e61020908d0e6c1304f6f0cabe7d6f2b3a8c03a2

---
+++ RNAseq/Expression/edgeR_analysis_combo.R
@@ -0,0 +1,20 @@
+library(edgeR)
+x <- read.delim("cuffnorm/genes.count_table",row.names="tracking_id")
+dim(x)
+group <- factor(c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                  2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+                  3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3))
+
+design <- model.matrix(~group)
+y <- DGEList(counts=x,group=group)
+
+y <- estimateGLMCommonDisp(y,design)
+y <- estimateGLMTrendedDisp(y,design)
+y <- estimateGLMTagwiseDisp(y,design)
+fit <- glmFit(y,design)
+lrt <- glmLRT(fit,coef=2)
+toptags <- topTags(lrt,n=200)
+write.csv(toptags,file="combo_toptags.csv")
+pdf("combo_edgeRplots.pdf")
+plotMDS(y)
+plotBCV(y)
