a7a9833a459facecde4d57e14767ed9353449b82
Jason Stajich <jason@bioperl.org>
2015-08-30 13:27:26 -0700

    swap order

--- annotation/Domains/make_HwGO.sh
+++ annotation/Domains/make_HwGO.sh
@@ -1,2 +1,2 @@
 
-awk '{print $2,"IEA",$4}' Hw2.all.maker.proteins.functional_remake.IPR.tsv.InterPro.association | perl -p -e 's/ /\t/g' > Hw2.GO.tab
+awk '{print $4,"IEA",$2}' Hw2.all.maker.proteins.functional_remake.IPR.tsv.InterPro.association | perl -p -e 's/ /\t/g' > Hw2.GO.tab
